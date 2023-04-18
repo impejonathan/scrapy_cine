100f9a95daf932c33fd074b51f088ac19c98e327
davidbreau <davidbreau@live.fr>
2023-04-18 10:34:07 +0200

    scrapping films ok

--- IMDb/IMDb/spiders/acteurs.py
+++ IMDb/IMDb/spiders/acteurs.py
@@ -4,7 +4,7 @@ from scrapy.spiders import CrawlSpider, Rule
 from ..items import ImdbItem
 
 class FilmSpider(CrawlSpider):
-    name = "films_top_250"
+    name = "films_top_250_only_actors"
     allowed_domains = ["www.imdb.com"]
     start_urls = ["https://www.imdb.com/chart/top/?ref_=nv_mv_250"]
     
@@ -27,17 +27,9 @@ class FilmSpider(CrawlSpider):
         # items = ImdbItem()
         yield {
         # for response in all_responses:
-        'titre' :  response.css('.sc-afe43def-1.fDTGTb::text').extract(),
-        'titre_original' : response.css('.sc-afe43def-3.EpHJp::text').extract(),
-        'score' : response.css('.sc-bde20123-1.iZlgcd::text').extract_first(),
-        'genre' : response.css('.ipc-chip__text::text').extract(),
-        'annee' : response.css('.ipc-link.ipc-link--baseAlt.ipc-link--inherit-color::text').extract_first().strip('()'),
-        'duree': response.css('.ipc-inline-list__item::text').extract_first(),
-        'descriptions' : response.css('.sc-5f699a2-1.cfkOAP::text').extract_first().strip(),
+        
+        # 'acteurs' : response.css('.ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link::text').extract(),
         'acteurs' : response.xpath("(//li[@data-testid='title-pc-principal-credit'])[last()]//a/text()")[1:].extract(),
-        'public' : response.css('.ipc-link.ipc-link--baseAlt.ipc-link--inherit-color::text').extract_first(),
-        'pays' : response.css('.ipc-metadata-list-item__list-content-item.ipc-metadata-list-item__list-content-item--link::text').extract()[-1].strip(),
-        'langue' : response.css('.ipc-metadata-list-item__list-content-item.ipc-metadata-list-item__list-content-item--link::text').extract()[0].strip().strip('()')
         }
            
 
